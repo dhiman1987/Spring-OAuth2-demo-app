b6b0102d2ed56db9f0dea5edb6e7a1acfdf5b7b3
Dhiman <dhiman.talapatra@gmail.com>
2024-06-30 14:30:10 +0200

    initial commit

---
+++ src/main/java/com/dhimantalapatra/spring/oauth2/demo/app/SpringOAuth2DemoAppApplication.java
@@ -0,0 +1,13 @@
+package com.dhimantalapatra.spring.oauth2.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOAuth2DemoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOAuth2DemoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhimantalapatra/spring/oauth2/demo/app/SpringOAuth2DemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhimantalapatra.spring.oauth2.demo.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOAuth2DemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
